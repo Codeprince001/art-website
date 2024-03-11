5c4eedc7b33ec28f22ccee2838b3cc83e7ad9ec7
Codeprince001 <urangyorkwisdom@gmail.com>
2024-03-11 15:44:19 +0100

    Used react router for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import Home from './pages/Home';
 import { About, Navbar } from './components';
-import Video from './components/Video';
-import Services from './components/Services';
-import Adverticement from './components/Adverticement';
-import FAQ from './components/FAQ';
-import Newsletter from './components/Newsletter';
-import Copyright from './components/Copyright';
+import { Route, Routes } from 'react-router-dom';
+import { Roadmap, Home } from './pages';
 
 function App() {
 
   return (
     <>
       <div className='bg-gradient-to-b from-[#7D2AE7] to-[#FAF9FF]'>
-        <Navbar />
-        <Home />
-        <About />
-      </div>
-      <Video />
-      <Services />
-      <Adverticement />
-      <div className='bg-gradient-to-b  from-[#1E0734] to-[#FAF9FF]'>
-        <FAQ />
-        <Newsletter />
-        <Copyright />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/roadmap' element={<Roadmap />} />
+        </Routes>
       </div>
     </>
   );
