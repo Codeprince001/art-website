c5756b40eca325b02a86eba845fc5e7cf7ecdb45
Codeprince001 <urangyorkwisdom@gmail.com>
2024-03-11 23:27:08 +0100

    export for pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,10 @@
 import Navbar from "./Navbar";
 import About from "./About/About";
 import Newsletter from "./Newsletter";
+import VideoBrief from "./VideoBrief";
+import Services from "./Services";
+import ProductApp from "./ProductApp";
+import FAQ from "./FAQ";
+import Copyright from "./Copyright";
 
-export { Navbar, About, Newsletter };
+export { Navbar, About, VideoBrief, Services, ProductApp, FAQ, Newsletter, Copyright };
