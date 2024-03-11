fd4314a13120fa13046a6fc99ce10499cee05e36
Codeprince001 <urangyorkwisdom@gmail.com>
2024-03-11 23:15:46 +0100

    import asset files

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,24 +1,26 @@
 import { RiHome5Fill } from "react-icons/ri";
 import { BiSolidNetworkChart } from "react-icons/bi";
+import { PiGlobeBold } from "react-icons/pi";
+import { LiaDumbbellSolid } from "react-icons/lia";
 
 
 export const links = [
-  { name: 'About', to: '/about', },
-  { name: 'Roadmap', to: '/roadmap' },
-  { name: 'FAQs', to: '/faq' },
-  { name: 'APP', to: '/app' },
-  { name: 'Store', to: '/store' },
+  { name: 'About', to: 'about', },
+  { name: 'Roadmap', to: 'roadmap' },
+  { name: 'FAQs', to: 'faq' },
+  { name: 'APP', to: 'app' },
+  { name: 'Store', to: 'store' },
 ];
 
 export const Offers = [
   { title: "Find your tribe", icon: RiHome5Fill, description: "Connect with like-minded individuals who share your passion for art and creativity. As an Earthling, you'll be part of a global community that celebrates diversity, collaboration, and artistic expression." },
-  { title: "Empower your creativity", icon: BiSolidNetworkChart, description: "Earth provides a supportive and nurturing environment for artists to unleash their creative potential. Gain access to resources, workshops, and mentorship opportunities designed to help you grow as an artist and reach your full potential." },
-  { title: "Make a Difference", description: "By joining Earth, you're not just becoming a part of an art community; you're also contributing to a movement that seeks to transform the art world for the better. " },
-  { title: "A World of Possibilities", description: "Earth harnesses the power of technology to create a seamless and stress-free experience for both artists and buyers." },
+  { title: "Empower your creativity", icon: LiaDumbbellSolid, description: "Earth provides a supportive and nurturing environment for artists to unleash their creative potential. Gain access to resources, workshops, and mentorship opportunities designed to help you grow as an artist and reach your full potential." },
+  { title: "Make a Difference", icon: BiSolidNetworkChart, description: "By joining Earth, you're not just becoming a part of an art community; you're also contributing to a movement that seeks to transform the art world for the better. " },
+  { title: "A World of Possibilities", icon: PiGlobeBold, description: "Earth harnesses the power of technology to create a seamless and stress-free experience for both artists and buyers." },
 ];
 
 export const accordionItems = [
-  { title: "What is the Earth Community?", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat" },
+  { title: "What is the Earth Community?", description: "The Earth Community is a vibrant, inclusive, and empowering platform for artists and art enthusiasts. It aims to revolutionize the way we perceive and appreciate art by democratizing the art world, fostering collaboration and connection, empowering artists, and promoting inclusivity and authenticity" },
   { title: "How does Eart demonstrate the art world", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat" },
   { title: "What resources and opportunities does Earth provide for artists", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat" },
   { title: "Where can i sell my art?", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat" },
