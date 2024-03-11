951339ee88c31b13fbb0d4c40aef3e2955e016b5
Codeprince001 <urangyorkwisdom@gmail.com>
2024-03-11 23:21:06 +0100

    comp for Hero section

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -4,10 +4,10 @@ import { Background_1, Background_2, Background_3 } from '../assets';
 export default () => {
 
   return (
-    <div className='mx-auto p-8 md:flex md:gap-5 md:items-center md:justify-center flex items-center relative  flex-1 w-full'>
-      <div className='' ><img src={Background_3} /></div>
-      <div ><img src={Background_2} /></div>
+    <div className='mx-auto lg:p-8 p-2 md:flex md:gap-5 md:items-center md:justify-center flex items-center relative  flex-1 w-full'>
       <div><img src={Background_1} /></div>
+      <div ><img src={Background_2} /></div>
+      <div className='' ><img src={Background_3} /></div>
     </div>
   );
 };
